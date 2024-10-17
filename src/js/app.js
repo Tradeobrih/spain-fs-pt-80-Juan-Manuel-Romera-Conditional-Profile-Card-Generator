ed1c43acec77ae83cb9b3f6e26440a7e89391a72
Tradeobrih <romera.valladolid@gmail.com>
2024-10-17 11:15:01 +0000

    primner cd

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,42 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+${cover}
+<img src="${variables.avatarURL}" class="photo" />
+<h1>
+${variables.name === null ? "Ryan" : variables.name} 
+${
+  variables.lastName === null ? "Boylett" : variables.lastName
+}                        
+</h1>
+<h2>${!variables.role ? "Web Developer" : variables.role}</h2>
+<h3>
+${variables.city === null ? "Miami" : variables.city}, 
+${variables.country === null ? "USA" : variables.country}
+</h3>
+<ul class="${variables.socialMediaPosition}">
+<li>
+  <a href="https://x.com/${
+    !variables.twitter ? "4geeksacademy" : variables.twitter
+  }"><span class="fab fa-twitter"></span></a>
+</li>
+<li>
+  <a href="https://github.com/${
+    !variables.github ? "4geeksacademy" : variables.github
+  }"><span class="fab fa-github"></span></a>
+</li>
+<li>
+  <a href="https://www.linkedin.com/${
+    !variables.linkedin ? "4geeksacademy" : variables.linkedin
+  }"><i class="fa fa-linkedin"></i></a></li>
+<li>
+  <a href="https://www.instagram.com/${
+    !variables.instagram ? "4geeksacademy" : variables.instagram
+  }"><span class="fab fa-instagram"></span></a>
+</li>
+</ul>
+</div>
+`;
 }
 
 /**
